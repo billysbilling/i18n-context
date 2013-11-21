beef15551e8e09d337dbffa611049858e2ab6612
Sebastian Seilund <sebastian@seilund.com>
2013-11-20 17:39:54 -0800

    added initial files

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        'billy-builder': {
+            title: 'i18n-module'
+            jshint: true
+        }
+    });
+
+    grunt.loadNpmTasks('billy-builder');
+};
